044fa7c6e459e7d196b89b882d5657b1d1cee903
pirathap-dev <pirathap.dev@gmail.com>
2025-07-04 16:37:41 +0530

    cors error 2 fixed

--- backend/server.js
+++ backend/server.js
@@ -64,7 +64,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-const allowedOrigins = ['https://rhiyaprinters.vercel.app', 'https://rhiyaprinters-admin.vercel.app', 'https://rhiyaprinters.ca'];
+const allowedOrigins = ['https://rhiyaprinters.vercel.app', 'https://rhiyaprinters-admin.vercel.app', 'https://www.rhiyaprinters.ca'];
 
 app.use(cors({
   origin: function(origin, callback){
